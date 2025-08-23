155104803e48e6e273eda5ac7d4268e73cf7061b
Matt Hosack <hosack.matt@gmail.com>
2025-08-23 16:03:59 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="loudstream",
-    version="0.3.0",
+    version="0.3.1",
     packages=["loudstream"],
     package_dir={"": "src"},
     cmake_install_dir="src/loudstream",
